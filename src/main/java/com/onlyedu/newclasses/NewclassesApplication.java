8faca8c36ae065ed2f0058e2edf26cad2152ef7e
Andy <472781184@qq.com>
2018-11-19 14:51:40 +0800

    first commit

---
+++ src/main/java/com/onlyedu/newclasses/NewclassesApplication.java
@@ -0,0 +1,12 @@
+package com.onlyedu.newclasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewclassesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewclassesApplication.class, args);
+    }
+}
